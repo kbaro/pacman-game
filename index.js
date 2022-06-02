e5b3476c21841554c0b96272fdefabad2782c0cc
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-02 22:29:16 +0200

    generating map boundaries

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.width = innerHeight
+
+class Boundary {
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    }
+}
+
+const boundaries = [
+    new Boundary({
+        position: {
+            x: 0,
+            y: 0
+        }
+    }), 
+    new Boundary({
+        position: {
+            x: 41,
+            y: 0
+        }
+    })
+]
+
+boundaries.forEach(boundary => {
+    boundary.draw()
+})
+
