579cda21e7e7947f1a3baec7eb0f41f04584edf7
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-04 14:54:03 +0200

    added score

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+const scoreEl = document.querySelector('#scoreEl')
+
 canvas.width = innerWidth
 canvas.height = innerHeight
 
@@ -88,6 +90,7 @@ const keys = {
 }
 
 let lastKey = ''
+let score = 0
 
 const map = [
     ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
@@ -413,6 +416,7 @@ function animate() {
         }
     }    
 
+    // touch pellets here
     for (let i = pellets.length - 1; 0 < i; i--) {
         const pellet = pellets[i]
         pellet.draw()
@@ -423,6 +427,8 @@ function animate() {
                         pellet.radius + player.radius
                         ) {
             pellets.splice(i, 1)
+            score += 10
+            scoreEl.innerHTML = score
         }
     }
     
