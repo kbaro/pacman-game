e0d437b65654125224c4877baa675de7b5e709ad
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-04 14:37:44 +0200

    generated pellets

--- index.js
+++ index.js
@@ -45,6 +45,22 @@ class Player {
     }
 }
 
+class Pellet {
+    constructor({ position }) {
+        this.position = position
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'white'
+        c.fill()
+        c.closePath()
+    }
+}
+
+const pellets = []
 const boundaries = []
 const player = new Player({
     position : {
@@ -397,6 +413,9 @@ function animate() {
         }
     }    
 
+    pellets.forEach((pellet) => {
+        pellet.draw()
+    })
     boundaries.forEach((boundary) => {
         boundary.draw()
 
