5e63afb857b0e809fb3102472a84197f5b9d5830
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-04 13:13:19 +0200

    collision detection

--- index.js
+++ index.js
@@ -100,10 +100,19 @@ function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height)
     boundaries.forEach((boundary) => {
         boundary.draw()
+
+        if (player.position.y - player.radius + player.velocity.y <= boundary.position.y + boundary.height && 
+            player.position.x + player.radius + player.velocity.x >= boundary.position.x && 
+            player.position.y + player.radius + player.velocity.y >= boundary.position.y && 
+            player.position.x - player.radius + player.velocity.x <= boundary.position.x + boundary.width
+            ) {
+                player.velocity.x = 0
+                player.velocity.y = 0
+        }
     })
     player.update()
-    player.velocity.y = 0
-    player.velocity.x = 0
+    // player.velocity.y = 0
+    // player.velocity.x = 0
 
 
     if (keys.w.pressed && lastKey === 'w') {
