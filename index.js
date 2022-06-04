069e7cebe56443f270d0d83fd0d85259640f94cd
Kristijan Baro <baro.kristijan@gmail.com>
2022-06-04 14:45:34 +0200

    removing pellets on collision

--- index.js
+++ index.js
@@ -413,9 +413,19 @@ function animate() {
         }
     }    
 
-    pellets.forEach((pellet) => {
+    for (let i = pellets.length - 1; 0 < i; i--) {
+        const pellet = pellets[i]
         pellet.draw()
-    })
+
+        if (Math.hypot(pellet.position.x - player.position.x,
+                        pellet.position.y - player.position.y
+                        ) < 
+                        pellet.radius + player.radius
+                        ) {
+            pellets.splice(i, 1)
+        }
+    }
+    
     boundaries.forEach((boundary) => {
         boundary.draw()
 
